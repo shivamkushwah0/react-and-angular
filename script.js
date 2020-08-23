9f2e43823ad613b947d14379472ffd8cefcc7529
Shivam <khushwahashubham0@gmail.com>
2020-08-23 18:08:16 +0530

    assignment 4

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+            $("#mycarousel").carousel({
+                interval:2000
+            });
+             $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+
+             $("#login").click(function(){
+                $("#loginmodal").modal('show');
+             });
+             $("#reserve").click(function(){
+                $("#reservemodal").modal('show');
+             });
+             $("#close-login-modal").click(function(){
+                $("#loginmodal").modal('hide');
+             });
+
+             $("#close-table-modal").click(function(){
+                $("#reservemodal").modal('hide');
+             });
+
+             
+        });
+
